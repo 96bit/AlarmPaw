01cecd2f96143f668233f1f9c50fb012f3297518
Alphonso Figliola <github@twown.com>
2024-01-28 16:51:30 +0800

    Update import-profile.sh

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+https://github.com/tsaohe/AlarmPaw/blob/main/.github/scripts/import-profile.sh#!/bin/bash
 
 set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "$PROVISIONING_PROFILE_PAW_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/paw.mobileprovision
-echo "$PROVISIONING_PROFILE_SERVER_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/server.mobileprovision
+echo "$PROVISIONING_PROFILE_SERVER_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/service.mobileprovision
 echo "$PROVISIONING_PROFILE_CONTENT_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/content.mobileprovision
